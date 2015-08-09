c3b86528dc030ea85badeb11a09910f4125177f5
Ricky JW <rickywid@hotmail.com>
2015-08-08 21:28:52 -0400

    login sessions added

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-	test "invalid signup information" do 
-		get signup_path
-		assert_no_difference 'User.count' do
-			post users_path, user: {
-				name: "",
-				email: "user@invalid",
-				password: "foo",
-				password_confirmation: "bar"
-			}
-
+	test "invalid signup information" do
+	    get signup_path
+	    assert_no_difference 'User.count' do
+	      post users_path, user: { name:  "",
+	                               email: "user@invalid",
+	                               password:              "foo",
+	                               password_confirmation: "bar" }
+	    end
+	    assert_template 'users/new'
+	  end
 
 		test "valid signup information" do
 			get signup_path
@@ -23,6 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 					password_confirmation:"password"
 				}
 		end
-		assert_template 'users/new'
+		assert_template 'users/show'
+		assert is_logged_in?
 	end
 end
